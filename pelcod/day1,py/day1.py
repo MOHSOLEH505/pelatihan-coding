7c19079e39f5a0ace3cc8b1cdcc8aca216ffb211
MOHSOLEH505 <ayamjagi69@gmail.com>
2024-09-07 11:02:51 +0700

    Add files via upload

---
+++ pelcod/day1,py/day1.py
@@ -0,0 +1,37 @@
+a = "apel" # "a" adalah variable dan "apel" adalah data
+print(a) 
+
+buah = "jeruk" #ini adalah tipedata 'string'
+harga = 15000 #ini adalah tipedata 'integer'
+berat = 1.5 #ini adalah  tipedata 'float'
+beli = True #ini adalah tipedata 'boolean'
+
+print(buah)
+print(harga)
+print(berat)
+print(beli)
+
+# format string
+nama = 'siti'
+umur = 16
+alamat = 'jl. buntu'
+tinggi = 1.75
+
+print(f"nama saya {nama}. saya berusia {umur} tahun, dan tinggi saya {tinggi} meter. saya tinggal di {alamat}")
+
+# input
+nama = input("siapa nama kamu?")
+print(f"halo, {nama}")
+
+umur = int(input("Umur kamu berapa?"))
+print(f"{umur} tahun")
+
+print("Halo, perkenalakn nama aku Bot!")
+print("Halo, Bot!")
+nama = input ("Nama, kamu siapa?")
+print(f"Namaku {nama}")
+print(f"Halo, {nama}. Salam kenal!")
+print(f"Kamu sibuk apa sekarang, {nama}?")
+kegiatan = input("kesbukanmu?")
+print(f"Aku lagi sibuk{kegiatan} sekarang")
+print(f"oke, Semoga {kegiatan} mu berhasil")
